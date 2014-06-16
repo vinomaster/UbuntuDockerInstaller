23e115000c789d5ff4c883db9c23c845dc6c5516
Dan Gisolfi <gisolfi@us.ibm.com>
2014-06-16 15:04:01 -0400

    Update udi.sh

--- udi.sh
+++ udi.sh
@@ -3,7 +3,7 @@
 # Provisioning instructions as per: 
 # http://docs.docker.io.s3-website-us-west-2.amazonaws.com/installation/ubuntulinux/#ubuntu-trusty-1404-lts-64-bit
 
-sudo apt-get -y -f update
+sudo apt-get update
 sudo apt-get -y -f install docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
